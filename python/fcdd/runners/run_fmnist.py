8e8ccd7811c7b6f0e9353a6e488e8c82be1fa1d7
jpcbertoldo <24547377+jpcbertoldo@users.noreply.github.com>
2022-01-17 14:31:33 +0100

    add cls-restrictions to run_fmnist

--- python/fcdd/runners/run_fmnist.py
+++ python/fcdd/runners/run_fmnist.py
@@ -7,6 +7,10 @@ class FmnistConfig(DefaultFmnistConfig):
     def __call__(self, parser):
         parser = super().__call__(parser)
         parser.add_argument('--it', type=int, default=5, help='Number of runs per class with different random seeds.')
+        parser.add_argument(
+            '--cls-restrictions', type=int, nargs='+', default=None,
+            help='Run only training sessions for some of the classes being nominal.'
+        )
         return parser
 
 
