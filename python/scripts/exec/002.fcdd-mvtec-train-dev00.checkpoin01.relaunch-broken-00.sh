7e8ee90570c1aa9e77fc60b846fb27d486656316
jpcbertoldo <24547377+jpcbertoldo@users.noreply.github.com>
2022-05-03 10:42:23 +0200

    update sbatch and exec scripts for pixel loss experiment

---
+++ python/scripts/exec/002.fcdd-mvtec-train-dev00.checkpoin01.relaunch-broken-00.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -e  # stops the execution of a script if a command or pipeline has an 
+
+THIS_SCRIPT_DIR=$( cd ${0%/*} && pwd -P )
+THIS_SCRIPT_DIR=$( realpath ${THIS_SCRIPT_DIR} )
+
+cd $HOME
+
+SBATCH_SCRIPT_FPATH="/cluster/CMM/home/jcasagrandebertoldo/repos/fcdd/python/scripts/sbatch/fcdd-mvtec-train-dev00-checkpoint01.sh"
+
+# debug params
+# --cls-restrictions 0 --it 1 --epochs 1 --no-test
+
+# // C) SUPervised, MASK, old loss
+# // "--supervise-mode", "noise", "--noise-mode", "mvtec_gt",
+# sbatch ${SBATCH_SCRIPT_FPATH} --supervise-mode noise --noise-mode mvtec_gt --cls-restrictions 4 5 6 7 8 9 10 11 12 13 14
+# sleep 3  # avoid the same directory name
+
+# // D) unsupervised (confetti), MASK, old loss
+# // "--supervise-mode", "malformed_normal_gt", "--noise-mode", "confetti",
+# sbatch ${SBATCH_SCRIPT_FPATH} --supervise-mode malformed_normal_gt --noise-mode confetti
+# sleep 3  # avoid the same directory name
+
+# // E) SUPervised, MASK, pixel level loss
+# // "--supervise-mode", "noise", "--noise-mode", "mvtec_gt", "--pixel-level-loss",
+# sbatch ${SBATCH_SCRIPT_FPATH} --supervise-mode noise --noise-mode mvtec_gt --pixel-level-loss --cls-restrictions 4 5 6 7 8 9 10 11 12 13 14
+# sleep 3  # avoid the same directory name
+
+# // F) unsupervised (confetti), MASK, pixel level loss
+# // "--supervise-mode", "malformed_normal_gt", "--noise-mode", "confetti", "--pixel-level-loss",
+# sbatch ${SBATCH_SCRIPT_FPATH} --supervise-mode malformed_normal_gt --noise-mode confetti --pixel-level-loss
+# sleep 3  # avoid the same directory name
+
+# ==================================================================================================================
+# ==================================================================================================================
+# ====================================================== FIX =======================================================
+# ==================================================================================================================
+# ==================================================================================================================
+
+# these use the fix as discussed by e-mail with philipp
+
+# // G) SUPervised, MASK, fixed loss
+# // "--supervise-mode", "noise", "--noise-mode", "mvtec_gt",
+# sbatch ${SBATCH_SCRIPT_FPATH} --supervise-mode noise --noise-mode mvtec_gt --pixel-loss-fix
+# sleep 3  # avoid the same directory name
+
+# // H) unsupervised (confetti), MASK, fixed loss
+sbatch ${SBATCH_SCRIPT_FPATH} --supervise-mode malformed_normal_gt --noise-mode confetti --pixel-loss-fix 
+# sleep 3  # avoid the same directory name
