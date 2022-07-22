046f8e8e54f704748849a5063c1092a400431254
jpcbertoldo <24547377+jpcbertoldo@users.noreply.github.com>
2022-07-22 17:43:33 +0200

    fix missing runs in u2netdd sweep 02

--- python/scripts/exec/035.u2net-mvtec-00-mising-sweep02.sh
+++ python/scripts/exec/035.u2net-mvtec-00-mising-sweep02.sh
@@ -1,38 +1,32 @@
 #!/usr/bin/env bash
 set -e  # stops the execution of a script if a command or pipeline has an 
-cd $HOME
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-SBATCH_SCRIPT_FPATH="/cluster/CMM/home/jcasagrandebertoldo/fcdd/python/scripts/sbatch/06-nparallel-arrayargs-01-1gpu-2cpuptask-3jobsimult.slurm"
+# ==============================================================================
+
+SBATCH_SCRIPT_FPATH="/cluster/CMM/home/jcasagrandebertoldo/fcdd/python/scripts/sbatch/06-nparallel-arrayargs-01-1gpu-2cpuptask.slurm"
 
 export SBATCH_SCRIPT_ARG_WORKDIR="${HOME}/fcdd/python/dev"
 export SBATCH_SCRIPT_ARG_CONDAENV="fcdd_rc21"
 
-# check me
-# check me
-# check me
-# check me
-# check me
-# check me
-export SBATCH_SCRIPT_ARG_SCRIPT_FNAME="train_mvtec_dev01.py"
+# NOT THE USUAL TRAIN SCRIPT!!!
+export SBATCH_SCRIPT_ARG_SCRIPT_FNAME="unetdd_mvtec_00_sweep_02_train_missingfromsweep.py"
 
 export SBATCH_SCRIPT_ARG_ARRAY_ARGS_FPATH="${SCRIPT_DIR}/035.u2net-mvtec-00-arrayargs"
-export SBATCH_SCRIPT_ARG_NPARALLEL_RUNS=3
+export SBATCH_SCRIPT_ARG_NPARALLEL_RUNS=1
 
-ARGS="--wandb-project fcdd-mvtec-bmvc --wandb-tags bmvc-01"
-ARGS="${ARGS} --n-seeds 1"
-
-# COMMON_ARGS=""
-# COMMON_ARGS="${COMMON_ARGS} --nworkers 1"
-# COMMON_ARGS="${COMMON_ARGS} --wandb-tags dev01 run-n-parallel-${NPARALLEL_RUNS}"
-# echo "COMMON_ARGS=${COMMON_ARGS}"
+ARGS=""
+ARGS="--wandb_tags run-n-parallel nparallel:${SBATCH_SCRIPT_ARG_NPARALLEL_RUNS}"
+ARGS="${ARGS} --wandb_tags fix-missing-sweep-02"
+echo "ARGS=${ARGS}"
 
 
 DEBUG_ARGS=""
-# DEBUG_ARGS="${DEBUG_ARGS} --n-seeds 1 --epochs 1"
-# DEBUG_ARGS="${DEBUG_ARGS} --wandb-offline --wandb-checkpoint-mode none"
-# DEBUG_ARGS="${DEBUG_ARGS} --wandb-tags debug"
-# DEBUG_ARGS="${DEBUG_ARGS} --wandb-tags deleteme"
+# DEBUG_ARGS="${DEBUG_ARGS} --epochs 1"
+# DEBUG_ARGS="${DEBUG_ARGS} --wandb_offline --wandb_checkpoint_mode none"
+# DEBUG_ARGS="${DEBUG_ARGS} --wandb_tags debug"
+# DEBUG_ARGS="${DEBUG_ARGS} --wandb_tags deleteme"
+# DEBUG_ARGS="${DEBUG_ARGS} --wandb_project mvtec-debug"
 echo "DEBUG_ARGS=${DEBUG_ARGS}"
 
 sbatch ${SBATCH_SCRIPT_FPATH} ${ARGS} ${DEBUG_ARGS}
